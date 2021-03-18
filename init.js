fd6ace7f8209139d8d5f52c5b721824c34fcc430
hyunhee <phhphh841@hanmail.net>
2021-03-18 18:06:15 +0900

    Modify:main.pug

--- init.js
+++ init.js
@@ -1,9 +1,11 @@
 import app from './app';
+import test from './test.json'
 
 const PORT = 4000;
 
 const handleListening = () =>
     console.log(`🌳Listenind On : http://localhost:${PORT}`);
-    //console.log(app)
+    console.log(test)
+    console.log(typeof(test))
 
 app.listen(PORT, handleListening)
